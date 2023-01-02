ac04ffb99a3594ebab2ca383facc01fbf75f7177
hieudt <dotrunghieu96@gmail.com>
2023-01-02 15:15:35 +0700

    update frontend

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -11,12 +11,15 @@ import {
   Icon,
   Input,
   Image,
-  Loader
+  Loader,
+  Comment,
+  Form
 } from 'semantic-ui-react'
 
-import { createTodo, deleteTodo, getComments, getTodos, patchTodo } from '../api/todos-api'
+import { addComments, createTodo, deleteTodo, getComments, getTodos, patchTodo } from '../api/todos-api'
 import Auth from '../auth/Auth'
 import { Todo } from '../types/Todo'
+import { TodoComment } from '../types/TodoComment'
 
 interface TodosProps {
   auth: Auth
@@ -28,6 +31,8 @@ interface TodosState {
   newTodoName: string
   loadingTodos: boolean
   showComments: number
+  commentItems: TodoComment[]
+  commentContent: string
 }
 
 export class Todos extends React.PureComponent<TodosProps, TodosState> {
@@ -35,7 +40,9 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
     todos: [],
     newTodoName: '',
     loadingTodos: true,
-    showComments: -1
+    showComments: -1,
+    commentItems: [],
+    commentContent: ''
   }
 
   handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -144,15 +151,7 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
     if (this.state.loadingTodos) {
       return this.renderLoading()
     }
-    let comment = null;
-    if (this.state.showComments) {
-      comment = 
-      <Grid.Column width={16}>
-        <Divider />
-      </Grid.Column>;
-    }
-
-    return this.renderTodosList(comment)
+    return this.renderTodosList()
   }
 
   renderLoading() {
@@ -165,10 +164,63 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
     )
   }
 
-  renderTodosList(comment: any) {
+  renderPostComment(todo: Todo) {
+    return (
+      <Grid.Column width={16}>
+        <Divider />
+        <Form comment>
+          <Form.TextArea
+            value={this.state.commentContent}
+            onChange={(event) => this.setState({ commentContent: event.target.value })}
+          />
+          <Button
+            floated='right'
+            content='Add Comment'
+            labelPosition='left'
+            icon='send'
+            primary
+            onClick={() => this.onAddComment(todo.todoId)}
+          />
+        </Form>
+      </Grid.Column>)
+  }
+
+  renderComments(): any {
+    console.log("rendering comments: " + this.state.commentItems.length);
+    let commentBlock: JSX.Element[] = [];
+    this.state.commentItems.forEach((commentItem) => {
+      commentBlock.push(
+        <Comment>
+          <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+          <Comment.Content>
+            <Comment.Author as='a'>You</Comment.Author>
+            <Comment.Metadata>
+              <span>{commentItem.createdAt}</span>
+            </Comment.Metadata>
+            <Comment.Text>
+              {commentItem.comment}
+            </Comment.Text>
+          </Comment.Content>
+        </Comment>
+      )
+    })
+    return (
+      <Comment.Group threaded>
+        {commentBlock}
+      </Comment.Group>
+    )
+  }
+
+  renderTodosList() {
     return (
       <Grid padded>
         {this.state.todos.map((todo, pos) => {
+          let comment = null;
+          let postComment = null;
+          if (this.state.showComments === pos) {
+            postComment = this.renderPostComment(todo);
+            comment = this.renderComments();
+          }
           return (
             <Grid.Row key={todo.todoId}>
               <Grid.Column width={1} verticalAlign="middle">
@@ -213,10 +265,13 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
               {todo.attachmentUrl && (
                 <Image src={todo.attachmentUrl} size="small" wrapped />
               )}
-              {comment}
+              {postComment}
               <Grid.Column width={16}>
                 <Divider />
               </Grid.Column>
+              <Grid.Column width={1}>
+              </Grid.Column>
+              {comment}
             </Grid.Row>
           )
         })}
@@ -224,13 +279,37 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
     )
   }
 
+  async onAddComment(todoId: string): Promise<void> {
+    console.log('add comment for todo:' + todoId);
+    console.log('comment:' + this.state.commentContent);
+    const new_comment: TodoComment = await addComments(
+      this.props.auth.getIdToken(),
+      todoId,
+      this.state.commentContent
+    );
+    const commentItems = this.addFirst(this.state.commentItems, new_comment);
+    this.setState({ commentItems: commentItems });
+    this.setState({ commentContent: '' });
+    console.log(this.state.commentItems);
+  }
+
+  addFirst(array: any[], item: any) {
+    return [
+      item,
+      ...array.slice(0)
+    ]
+  }
+
   async onCommentButtonClick(todoId: string, position: number): Promise<void> {
     console.log("show comments");
-    const commentItems = await getComments(this.props.auth.getIdToken(), todoId);
-    console.log(commentItems)
-    if (this.state.showComments == position) {
+    if (this.state.showComments === position) {
       this.setState({ showComments: -1 });
+      this.setState({ commentItems: [] });
+      this.setState({ commentContent: '' });
     } else {
+      const commentItems = await getComments(this.props.auth.getIdToken(), todoId);
+      console.log(commentItems)
+      this.setState({ commentItems: commentItems });
       this.setState({ showComments: position });
     }
   }
